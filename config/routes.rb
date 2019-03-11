189db8235649b3ef2e4c6e1ddbffdb23fefd36f2
Mallauy <mallaury.bajas1@gmail.com>
2019-03-11 14:18:30 +0100

    form contact added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'single_pages/contact'
   root 'home#index'
+  get 'contact', to: 'single_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
