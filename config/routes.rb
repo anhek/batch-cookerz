d38fcba34836565becbd3d4f7921665b0110f0a4
Mallauy <mallaury.bajas1@gmail.com>
2019-03-11 17:51:10 +0100

    routes and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
-  root 'home#index'
-  get 'contact', to: 'single_pages#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   root 'recipes#index'
   get 'contact', to: 'single_pages#contact'
   
@@ -16,7 +10,6 @@ Rails.application.routes.draw do
   resources :avatars, only: [:create]
 end
   
->>>>>>> 8b62ce67dec556ecab9b786c0f23dda43797917a
 end
 
 
