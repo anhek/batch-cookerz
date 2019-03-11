748dc1167c57447a43cf4343f91eab4bdf4eaba6
degregor69 <raphael.degregorio@gmail.com>
2019-03-11 14:56:02 +0100

    routes corrected

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :recipes, only: [:index]
   resources :ingredients, only: [:index]
-
   
-=======
   root 'home#index'
 
   resources :users, only: [:show, :edit]
   get 'contact', to: 'single_pages#contact'
->>>>>>> e7e37ff8db8f7124cb4b934fef666896c0edd39b
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
