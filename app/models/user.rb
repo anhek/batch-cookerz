b3a6d4882a42d2413a02583613543a26ed2bc03f
anhek <shynn@icloud.com>
2019-03-11 17:11:54 +0100

    adding sendgrid mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :recipes, through: :comments
   has_many :recipes, through: :likes
   has_many :menus
+
+  after_create :send_welcome_email_to_new_user, :send_new_user_email_to_admin
+
+  def send_welcome_email_to_new_user
+    UserMailer.welcome_email_to_new_user(self).deliver_now
+  end
+
+  def send_new_user_email_to_admin
+    AdminMailer.new_user_email_to_admin(self).deliver_now
+  end
+
 end
