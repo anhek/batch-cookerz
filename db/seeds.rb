1a9ac80e9052b1832151bad418aef895014d02b4
degregor69 <raphael.degregorio@gmail.com>
2019-03-11 13:56:57 +0100

    gem faker installed and recipes index without front installed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 5.times do |i|
-    User.create!(email: "email#{i+1}@gmail.com", password: "raph-le-boss")
+  User.create!(email: "email#{i+1}@gmail.com", password: "raph-le-boss")
 end
 puts "user cree"
-    RecipeCategory.create!(name: "recipe_category")
-    puts "recipecategory cree"
-    IngredientCategory.create!(name: "ingredient_category")
-    puts "ingredientcategory cree"
-    Recipe.create!(name: "recipe 1", recipe_category_id: 1)
-    puts "recipe cree"
-    Menu.create!(number_of_recipes: 1, user_id: 1)
-    puts "menu cree"
-    MenuRecipe.create!(menu_id: 1, recipe_id:1)
-    puts "menurecipe cree"
-    Ingredient.create!(name: "ingredient 1", ingredient_category_id: 1)
-    puts "ingredient cree"
-    Composition.create(recipe_id: 1, ingredient_id: 1)
-    puts "composition cree"
-    Comment.create(recipe_id: 1, description: "comment 1", user_id: 1)
-    puts "comment cree"
-    Like.create(recipe_id: 1, user_id: 1)
-    puts "like cree"
+	RecipeCategory.create!(name: "recipe_category")
+	puts "recipecategory cree"
+	IngredientCategory.create!(name: "ingredient_category")
+	puts "ingredientcategory cree"
+	
+
+5.times do 
+	Recipe.create!(name: Faker::Food.dish , description: Faker::Food.description, preparation_time: rand(10..25), cooking_time: rand(10..35), cost: (5..10), recipe_category_id: 1)
+end
+
+	puts "recipe cree"
+	Menu.create!(number_of_recipes: 1, user_id: 1)
+	puts "menu cree"
+	MenuRecipe.create!(menu_id: 1, recipe_id:1)
+	puts "menurecipe cree"
+	Ingredient.create!(name: "ingredient 1", ingredient_category_id: 1)
+	puts "ingredient cree"
+	Composition.create(recipe_id: 1, ingredient_id: 1)
+	puts "composition cree"
+	Comment.create(recipe_id: 1, description: "comment 1", user_id: 1)
+	puts "comment cree"
+	Like.create(recipe_id: 1, user_id: 1)
+	puts "like cree"
