529b1376db50d79fb1c767120cc76fba1fc9b0f5
Laurane01 <petitlaurane01@gmail.com>
2019-03-11 11:21:52 +0100

    ingredient table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_101834) do
+ActiveRecord::Schema.define(version: 2019_03_11_102054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 2019_03_11_101834) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "calories"
+    t.decimal "price"
+    t.bigint "ingredient_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_category_id"], name: "index_ingredients_on_ingredient_category_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -99,6 +109,7 @@ ActiveRecord::Schema.define(version: 2019_03_11_101834) do
 
   add_foreign_key "comments", "recipes"
   add_foreign_key "comments", "users"
+  add_foreign_key "ingredients", "ingredient_categories"
   add_foreign_key "likes", "recipes"
   add_foreign_key "likes", "users"
   add_foreign_key "menu_recipes", "menus"
