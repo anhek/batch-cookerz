10ed8db7cca221a15b74ca9f87fb243d8458e9a3
Mallauy <mallaury.bajas1@gmail.com>
2019-03-11 15:02:53 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-<<<<<<< HEAD
-
-  resources :users, only: [:show, :edit]
-=======
   get 'contact', to: 'single_pages#contact'
->>>>>>> 1dad38a6d17bf5ae533b9eeaacc37b9fe1e68bc2
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
