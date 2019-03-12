e9a10883da2eb1f3e36df932689dc8a6f2b38593
degregor69 <raphael.degregorio@gmail.com>
2019-03-12 15:11:13 +0100

    checkboxes for ingredients working and values usables in the controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,10 +8,27 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    recipe = Recipe.new()
+    @ingredients = []
+    params[:recipe][:ingredient].each do |ingredient|
+      if ingredient != '0'
+        @ingredients << ingredient.to_i
+      end
+    end
+    print "La liste des ingrédients est #{@ingredients}"
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     @comments = Comment.where(recipe_id: @recipe.id)
     @comment = Comment.new
   end 
   
+  private 
+
+  def recipe_params
+    puts
+    params[:recipe].permit(:name, :description, :preparation_time, :cooking_time)
+  end
 end
