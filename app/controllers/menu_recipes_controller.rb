c01309e0e14d3611cf6ffff3dd2f83c361c5e1c1
degregor69 <raphael.degregorio@gmail.com>
2019-03-14 11:05:11 +0100

    route created, link put in index, needs to be modified to work

--- app/controllers/menu_recipes_controller.rb
+++ app/controllers/menu_recipes_controller.rb
@@ -1,4 +1,7 @@
 class MenuRecipesController < ApplicationController
   def create
+    puts '$' * 60
+    puts params.inspect
+    redirect_to recipes_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'menu_recipes/create'
   root 'home#index'
   get 'contact', to: 'single_pages#contact'
   get 'concept', to: 'single_pages#concept'
   get 'team', to: 'single_pages#team'
   
   devise_for :users
+  resources :ingredients, only: [:index] 
+  resources :menu_recipes, only: [:index, :create]
+
   resources :recipes, only: [:index, :show, :new, :create] do
     resources :comments, only: [:create, :destroy]
     resources :likes, only: [:create, :destroy]
@@ -16,8 +18,7 @@ Rails.application.routes.draw do
     resources :menus, only: [:show]
   end
 
-  resources :ingredients, only: [:index] 
-  resources :menu_recipes, only: [:create]
+
   
 end
 
