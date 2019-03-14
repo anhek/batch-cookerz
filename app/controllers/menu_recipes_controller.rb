5d2ea0c2c364cb3b53efc392461c018b9c867d72
degregor69 <raphael.degregorio@gmail.com>
2019-03-14 11:26:07 +0100

    create method for menu_recipe working from index

--- app/controllers/menu_recipes_controller.rb
+++ app/controllers/menu_recipes_controller.rb
@@ -1,7 +1,16 @@
 class MenuRecipesController < ApplicationController
+ 
   def create
     puts '$' * 60
     puts params.inspect
-    redirect_to recipes_path
+    menu_recipe = MenuRecipe.new(recipe_id: params[:recipe_id], menu_id: params[:menu_id])
+    
+    if menu_recipe.save
+      puts "La recette a bien été ajoutée à ton menu !"
+      redirect_to recipes_path
+    else
+      puts "Désolé, la recette n'a pas été ajoutée !"
+      redirect_to recipes_path
+    end
   end
 end
