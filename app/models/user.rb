345ec3eb35a0d7df9df6215a3281f3a390fea1f9
Laurane01 <petitlaurane01@gmail.com>
2019-03-14 15:40:51 +0100

    default picture added to comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,34 +16,34 @@ class User < ApplicationRecord
 
   has_many :likes, dependent: :destroy
   
-  # Email validation
-  validates :email, 
-  presence: true,
-  uniqueness: true,
-  format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+  # # Email validation
+  # validates :email, 
+  # presence: true,
+  # uniqueness: true,
+  # format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
 
-  # Password validation
-  PASSWORD_FORMAT = /\A
-  (?=.{8,})          # Must contain 8 or more characters
-  (?=.*\d)           # Must contain a digit
-  # (?=.*[a-z])        # Must contain a lower case character
-  # (?=.*[A-Z])        # Must contain an upper case character
-  # # (?=.*[[:^alnum:]]) # Must contain a symbol
-  /x
+  # # Password validation
+  # PASSWORD_FORMAT = /\A
+  # (?=.{8,})          # Must contain 8 or more characters
+  # (?=.*\d)           # Must contain a digit
+  # # (?=.*[a-z])        # Must contain a lower case character
+  # # (?=.*[A-Z])        # Must contain an upper case character
+  # # # (?=.*[[:^alnum:]]) # Must contain a symbol
+  # /x
 
-  validates :password, 
-    presence: true, 
-    length: { in: Devise.password_length }, 
-    format: { with: PASSWORD_FORMAT }, 
-    confirmation: true, 
-    on: :create 
+  # validates :password, 
+  #   presence: true, 
+  #   length: { in: Devise.password_length }, 
+  #   format: { with: PASSWORD_FORMAT }, 
+  #   confirmation: true, 
+  #   on: :create 
 
-  validates :password, 
-    allow_nil: true, 
-    length: { in: Devise.password_length }, 
-    format: { with: PASSWORD_FORMAT }, 
-    confirmation: true, 
-    on: :update
+  # validates :password, 
+  #   allow_nil: true, 
+  #   length: { in: Devise.password_length }, 
+  #   format: { with: PASSWORD_FORMAT }, 
+  #   confirmation: true, 
+  #   on: :update
 
   def send_welcome_email_to_new_user
     UserMailer.welcome_email_to_new_user(self).deliver_now
