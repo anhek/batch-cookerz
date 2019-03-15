d8c0b23c319431bb4145031a51efe26dad3fa076
Laurane01 <petitlaurane01@gmail.com>
2019-03-15 13:01:53 +0100

    merging ok

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,4 +3,6 @@ class Menu < ApplicationRecord
     belongs_to :user
     has_many :recipes, through: :menu_recipes
     
+    validates :user, presence: true
+    validates :recipes, presence: true
 end
