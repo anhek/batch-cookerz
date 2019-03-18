2034948e92b8e96e32cc2356b2ff17cef2f73206
degregor69 <raphael.degregorio@gmail.com>
2019-03-18 16:34:53 +0100

    link to create the shopping list added

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,2 +1,12 @@
 class ShoppingListsController < ApplicationController
+  def create
+    puts "Bonjour et bienvenue dans le create de shopping list"
+    menu = Menu.find(params[:menu_id])
+    menu_recipes = MenuRecipe.where(menu_id: menu.id)
+    
+    recipes = []
+    menu_recipes.each do |menu_recipe|
+      
+    end
+  end
 end
