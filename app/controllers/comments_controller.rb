b9496b326cf645b045e9e88fe44f6870ad13eb75
Mallauy <mallaury.bajas1@gmail.com>
2019-03-18 14:09:53 +0100

    delete comment on recipe

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
-    def create
-        @recipe = Recipe.find(params[:recipe_id])
-        comment = Comment.new(user_id: current_user.id, recipe_id: @recipe.id, description: params[:comment][:description])
-        if comment.save
-        redirect_to recipe_path(@recipe)
-        end 
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    comment = Comment.new(user_id: current_user.id, recipe_id: @recipe.id, description: params[:comment][:description])
+    if comment.save
+      redirect_to recipe_path(@recipe)
+    end 
   end 
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to recipe_path(@recipe)
+  end
 
 end
