d7cee8bdd3cac21a3b1a330612bd6f407e83ef8c
Mallauy <mallaury.bajas1@gmail.com>
2019-03-18 14:30:14 +0100

    comment delete button only for author in controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-
+  before_action :is_my_comment!, only: [:destroy]
+  
   def create
     @recipe = Recipe.find(params[:recipe_id])
     comment = Comment.new(user_id: current_user.id, recipe_id: @recipe.id, description: params[:comment][:description])
@@ -10,10 +11,22 @@ class CommentsController < ApplicationController
   end 
 
   def destroy
+    puts params.inspect
     @recipe = Recipe.find(params[:recipe_id])
-    @comment = Comment.find(params[:id])
+    @comment = Comment.find(params[:comment_id])
     @comment.destroy
     redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def is_my_comment!
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = Comment.find(params[:comment_id])
+    if @comment.user != current_user 
+      flash[:error] = "Supprime pas les commentaires des autres fdp"
+      redirect_to recipe_path(@recipe)
+    end
   end
 
 end
