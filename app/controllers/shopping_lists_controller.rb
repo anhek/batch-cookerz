bc642f86f62f994f2111528c6b239b8b4f2c36f1
degregor69 <raphael.degregorio@gmail.com>
2019-03-19 11:26:38 +0100

    number_of_different_ingredients method put in shopping list model to empty shopping list controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,31 +1,13 @@
 class ShoppingListsController < ApplicationController
   def create
+    @shopping_list = ShoppingList.new
     menu = Menu.find(params[:menu_id])
-    menu_recipes = MenuRecipe.where(menu_id: menu.id)
-    
-    recipes = []
-    menu_recipes.each do |menu_recipe|
-      recipes << Recipe.find(menu_recipe.recipe_id)
-    end
+    grouped_ingredients = @shopping_list.find_number_of_different_ingredients_in_a_menu(menu)
 
-    compositions = []
-    recipes.each do |recipe|
-      compositions << Composition.where(recipe_id: recipe.id)
-    end
-
-    compositions = compositions.flatten
-
-    @ingredients = []
-    compositions.each do |composition|
-      @ingredients << Ingredient.find_by(id: composition.ingredient_id)
-    end
-    recipes.each do |recipe|
-    end
-
-    @grouped_ingredients = @ingredients.group_by(&:id)
-    @shopping_list = ShoppingList.new(menu_id: menu.id, ingredients_qty: @grouped_ingredients.count)
+    @shopping_list = ShoppingList.new(menu_id: menu.id, ingredients_qty: grouped_ingredients.count)
     if @shopping_list.save
-      puts "C'est bon, c'est sauvegardé !"
+      flash[:success] = "La liste de courses a bien été créée !"
+      redirect_to root_path
     else 
       puts "Oups, petit problème !"
     end
--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,3 +1,31 @@
 class ShoppingList < ApplicationRecord
   belongs_to :menu
+
+
+
+  def find_number_of_different_ingredients_in_a_menu(menu)
+    menu_recipes = MenuRecipe.where(menu_id: menu.id)
+    
+    recipes = []
+    menu_recipes.each do |menu_recipe|
+      recipes << Recipe.find(menu_recipe.recipe_id)
+    end
+
+    compositions = []
+    recipes.each do |recipe|
+      compositions << Composition.where(recipe_id: recipe.id)
+    end
+
+    compositions = compositions.flatten
+
+    ingredients = []
+    compositions.each do |composition|
+      ingredients << Ingredient.find_by(id: composition.ingredient_id)
+    end
+    recipes.each do |recipe|
+    end
+
+    grouped_ingredients = ingredients.group_by(&:id)
+    return grouped_ingredients
+  end
 end
