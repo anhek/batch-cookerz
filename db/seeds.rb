43bcd1a74d0330db9e45ae81346d9eacfc8f7943
Mallauy <mallaury.bajas1@gmail.com>
2019-03-20 16:07:35 +0100

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -338,6 +338,50 @@ end
 
 Ingredient.create!(name: "Carotte", calories: rand(50..250), ingredient_category_id: rand(1..5))
 Ingredient.create!(name: "Patate", calories: rand(50..250), ingredient_category_id: rand(1..5))
+Recipe.create!(name: "Lasagnes à l'italienne" , description: "Etape 1
+
+Faites revenir 2 minutes à feu moyen le haché de Veau Tendriade.
+Etape 2
+
+Dans un bol, mélangez le coulis de tomates et la ricotta. Salez et poivrez.
+Etape 3
+
+Versez un filet d’huile d’olive dans le fond d’un plat à gratin. Déposez une couche de pâte, puis ajouter successivement une couche de haché de veau, une couche de coulis, et une couche d’origan et basilic ciselé. Recommencez l’opération en terminant par le coulis.
+Etape 4
+
+Ajoutez du parmesan et de l’emmental râpé pour gratiner. Passer au four à 240°C (Thermostat 8) pendant environ 20 min.", preparation_time: 15, cooking_time: 25, price_indicator: 1, recipe_category_id: 1, user_id:3)
+
+
+puts "Recipe created"
+
+3.times do |i|
+	MenuRecipe.create!(menu_id: 1, recipe_id:i+1)
+end
+
+Ingredient.create!(name: "Carotte", calories: rand(50..250), ingredient_category_id: rand(1..5))
+Ingredient.create!(name: "Patate", calories: rand(50..250), ingredient_category_id: rand(1..5))
+Recipe.create!(name: "Lasagnes à l'italienne" , description: "Etape 1
+
+Faites revenir 2 minutes à feu moyen le haché de Veau Tendriade.
+Etape 2
+
+Dans un bol, mélangez le coulis de tomates et la ricotta. Salez et poivrez.
+Etape 3
+
+Versez un filet d’huile d’olive dans le fond d’un plat à gratin. Déposez une couche de pâte, puis ajouter successivement une couche de haché de veau, une couche de coulis, et une couche d’origan et basilic ciselé. Recommencez l’opération en terminant par le coulis.
+Etape 4
+
+Ajoutez du parmesan et de l’emmental râpé pour gratiner. Passer au four à 240°C (Thermostat 8) pendant environ 20 min.", preparation_time: 15, cooking_time: 25, price_indicator: 1, recipe_category_id: 1, user_id:3)
+
+
+puts "Recipe created"
+
+3.times do |i|
+	MenuRecipe.create!(menu_id: 1, recipe_id:i+1)
+end
+
+Ingredient.create!(name: "Carotte", calories: rand(50..250), ingredient_category_id: rand(1..5))
+Ingredient.create!(name: "Patate", calories: rand(50..250), ingredient_category_id: rand(1..5))
 15.times do 
   Ingredient.create!(name: Faker::Food.ingredient, calories: rand(50..250), ingredient_category_id: rand(1..5))
 end
