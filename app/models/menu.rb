1d6e5a559317d3985ab697148b06a469480910bd
degregor69 <raphael.degregorio@gmail.com>
2019-03-21 14:09:12 +0100

    number of recipes changed by number of people in menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,12 +1,17 @@
 class Menu < ApplicationRecord
+  before_save :default_values
 
-    belongs_to :user
-    has_many :menu_recipes, dependent: :destroy
-    has_one :shopping_list, dependent: :destroy
-    has_many :recipes, through: :menu_recipes
-    has_one :shopping_list, dependent: :destroy 
-    
-    
-    validates :user, presence: true
-    # validates :recipes, presence: true
+  belongs_to :user
+  has_many :menu_recipes, dependent: :destroy
+  has_one :shopping_list, dependent: :destroy
+  has_many :recipes, through: :menu_recipes
+  has_one :shopping_list, dependent: :destroy 
+  
+  
+  validates :user, presence: true
+  
+  def default_values
+    self.number_of_people ||= 1
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,24 +1,24 @@
 class Recipe < ApplicationRecord
+  before_save :default_values
+  belongs_to :recipe_category
+  belongs_to :user
+  has_many :users, through: :likes
+  has_many :comments
+  has_many :users, through: :comments
+  has_many :menus, through: :menu_recipes
+  has_many :compositions
+  has_many :ingredients, through: :compositions
+  has_many :likes, dependent: :destroy
+  has_one_attached :picture
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :price_indicator, presence: true
+  validates :recipe_category_id, presence: true
+
     
-    belongs_to :recipe_category
-    belongs_to :user
-    has_many :users, through: :likes
-    has_many :comments
-    has_many :users, through: :comments
-    has_many :menus, through: :menu_recipes
-    has_many :compositions
-    has_many :ingredients, through: :compositions
-    has_many :likes, dependent: :destroy
-    has_one_attached :picture
-
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :preparation_time, presence: true
-    validates :cooking_time, presence: true
-    validates :price_indicator, presence: true
-    validates :recipe_category_id, presence: true
-
-    before_save :default_values
 
   
   def get_ingredients_number_from_new_recipe_form(params)
