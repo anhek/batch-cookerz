6144909ca5ef34a764695ebbe584d45903ce18dc
degregor69 <raphael.degregorio@gmail.com>
2019-03-21 11:53:46 +0100

    index recipes displayed

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,2 +1,15 @@
 class Admin::RecipesController < ApplicationController
+  before_action :check_if_admin 
+  
+  def index
+    @recipes = Recipe.all
+  end
+
+  def check_if_admin
+    if user_signed_in?
+      current_user.is_admin == true
+    end
+ end
+
+
 end
