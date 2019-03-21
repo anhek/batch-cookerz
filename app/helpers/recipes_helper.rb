e649f57913e720c4c88cc5b04103070795f13507
degregor69 <raphael.degregorio@gmail.com>
2019-03-21 20:56:53 +0100

    problem with editing recipe fixed in view

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,8 +1,11 @@
 module RecipesHelper
 
-  def is_current_user_the_creator?
-    recipe = @recipe
-    current_user.id == recipe.user_id
+  def is_current_user_the_creator(recipe)
+    if user_signed_in?
+      if current_user.id == recipe.user_id
+        return true
+      end
+    end
   end
 
 end
