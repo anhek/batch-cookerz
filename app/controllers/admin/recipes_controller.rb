d2fbd879424b33ebb2c8b01fc9cc01d3b71a54a2
degregor69 <raphael.degregorio@gmail.com>
2019-03-22 17:37:54 +0100

    all admin powers working

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -45,9 +45,13 @@ class Admin::RecipesController < ApplicationController
   end
 
   def check_if_admin!
-    if current_user.is_admin == false
+    if user_signed_in?
+      if current_user.is_admin == false
+        flash[:error] = "Accès réservé aux meilleurs cuisiniers !"
+        redirect_to root_path
+      end
+    else
       flash[:error] = "Accès réservé aux meilleurs cuisiniers !"
-      
       redirect_to root_path
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -242,6 +242,10 @@ end
 puts "Composition created"
 
 Comment.create(recipe_id: 1, description: "Super ! J'adore cette recette :)", user_id: 1)
+
+3.times do |i|
+  Comment.create(recipe_id: 1, description: "Super ! J'adore cette recette :)", user_id: i+1)
+end
 puts "Comment created"
 
 Like.create(recipe_id: 1, user_id: 1)
